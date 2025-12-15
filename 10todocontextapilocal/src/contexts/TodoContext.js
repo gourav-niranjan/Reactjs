609686c0adc36f2d51ea519b2d3ecae6731155ac
gourav-niranjan <gouravniranjan9@gmail.com>
2025-12-15 19:59:07 +0530

    ToDo project based on context API with local storage

---
+++ 10todocontextapilocal/src/contexts/TodoContext.js
@@ -0,0 +1,20 @@
+import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[{
+        id: 1,
+        todo: "to do 1",
+        completed: false
+    }],
+    addTodo: ( todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleTodo: (id) => {}
+})
+
+ export const useTodo = () =>{
+    return useContext(TodoContext)
+ }
+
+export const TodoContextProvider = TodoContext.Provider
+
